43bc80f92c732815016eee386e42d0d2962a3392
Michaelhybrid <michaeloludare15@gmail.com>
2023-01-31 13:11:00 +0100

    Gramma

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,15 +3,15 @@ import searchIcon from './images/search.svg';
 const Search = (props) => {
   return (
     <div className="Search">
-        <p className="input__prompt">What word are you seeking?</p>
-        <div className="input-container">
-            <input type="text" placeholder="time" className="search__input"
-            name="word"
-            onChange={props.handleChange}
-            value={props.formData.message}
-            />
-            <div className="search-icon" onClick={props.getWord}><img src={searchIcon}/></div>
-        </div>
+      <p className="input__prompt">What word do you seek?</p>
+      <div className="input-container">
+        <input type="text" placeholder="time" className="search__input"
+          name="word"
+          onChange={props.handleChange}
+          value={props.formData.message}
+        />
+        <div className="search-icon" onClick={props.getWord}><img src={searchIcon} /></div>
+      </div>
     </div>
   )
 }
